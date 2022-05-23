7574fc79eae59c47ceca70a212f72ef5b9db8e94
Mike Cavaliere <mcavaliere@echobind.com>
2022-05-23 16:00:46 -0400

    feat: base transitions

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,6 +3,13 @@ import { extendTheme } from "@chakra-ui/react";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
+  styles: {
+    global: {
+      "*": {
+        transition: "0.3s all ease-in-out",
+      },
+    },
+  },
   colors: {
     brand: {
       100: "#f7fafc",
