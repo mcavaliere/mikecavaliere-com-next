3a5be0a9e71f9be459df769492cf718771efbc25
Mike Cavaliere <mcavaliere@echobind.com>
2022-05-25 12:42:18 -0400

    feat: Google Analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
+
 import { theme } from "../lib/theme";
 import { getLayout as getSiteOuterLayout } from "layouts/SiteOuterLayout";
 import { AppPropsWithLayout } from "lib/types";
+import { useAnalytics } from "lib/analytics";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Grab the layout if set; otherwise default to the default layout.
   const getLayout = Component.getLayout || getSiteOuterLayout;
+
+  // Report pageviews and events to Google Analytics.
+  useAnalytics();
 
   return (
     <ChakraProvider theme={theme}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,29 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                    });
+                `,
+                }}
+              />
+            </>
+          ) : null}
+        </Head>
         <body>
           <ColorModeScript />
           <Main />
