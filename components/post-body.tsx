171fa55d6ac7fdbeacf298c61a364adeb1a8b3dc
Mike Cavaliere <mcavaliere@echobind.com>
2022-06-23 15:43:19 -0400

    refactor: break node renderer into function

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -4,28 +4,26 @@ import {
   PostRendererProps,
 } from "lib/postRenderers";
 
+export function renderNode(nodeProps) {
+  const { tagName, textContent } = nodeProps;
+  const rendererProps: PostRendererProps = {
+    children: textContent,
+    key: textContent,
+  };
+
+  const tagRenderer = rendererMap[tagName];
+
+  if (tagName === "GIST") {
+    rendererProps.meta = nodeProps.meta;
+  }
+
+  if (tagRenderer) {
+    return rendererMap[tagName](rendererProps);
+  } else {
+    return DefaultRenderer({ children: textContent });
+  }
+}
+
 export default function PostBody({ contentMap }) {
-  return (
-    <>
-      {contentMap.map((nodeProps) => {
-        const { tagName, textContent } = nodeProps;
-        const rendererProps: PostRendererProps = {
-          children: textContent,
-          key: textContent,
-        };
-
-        const tagRenderer = rendererMap[tagName];
-
-        if (tagName === "GIST") {
-          rendererProps.meta = nodeProps.meta;
-        }
-
-        if (tagRenderer) {
-          return rendererMap[tagName](rendererProps);
-        } else {
-          DefaultRenderer({ children: textContent });
-        }
-      })}
-    </>
-  );
+  return <>{contentMap.map(renderNode)}</>;
 }
