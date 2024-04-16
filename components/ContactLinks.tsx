a70520cd966fe70a03e8ef5f0b0011818ce8acb1
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-04-16 13:06:49 -0400

    feat: ignore .env, make contact icons more visible

--- components/ContactLinks.tsx
+++ components/ContactLinks.tsx
@@ -1,30 +1,36 @@
 import { HStack, ButtonGroup, IconButton } from "@chakra-ui/react";
-import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export function ContactLinks() {
   return (
     <HStack marginY={10} marginX="auto">
       <ButtonGroup size="sm">
         <IconButton
+          size="lg"
           as="a"
           aria-label="Follow me on Twitter"
-          icon={<FaTwitter />}
+          icon={<FaTwitter color="#fff" />}
           href="https://twitter.com/mcavaliere"
           target="_blank"
+          bg="#2BC2FA"
         />
         <IconButton
+          size="lg"
           as="a"
-          aria-label="Follow me on LinkedImn"
-          icon={<FaLinkedin />}
+          aria-label="Follow me on LinkedIn"
+          icon={<FaLinkedinIn color="#fff" />}
           href="https://www.linkedin.com/in/mikecavaliere"
           target="_blank"
+          bg="#0077b5"
         />
         <IconButton
+          size="lg"
           as="a"
           aria-label="Follow me on GitHub"
-          icon={<FaGithub />}
+          icon={<FaGithub color="#fff" />}
           href="https://github.com/mcavaliere"
           target="_blank"
+          bg="#24292e"
         />
       </ButtonGroup>
     </HStack>
