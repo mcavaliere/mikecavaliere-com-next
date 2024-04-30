2a9eb5a733a4f9ada8a6c6517388dbda8d30df08
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-04-30 15:45:51 -0400

    feat: add existing renderers to MDX

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,14 @@
-import type { MDXComponents } from 'mdx/types'
+import type { MDXComponents } from "mdx/types";
+import { rendererMap } from "./lib/postRenderers";
+
+const rendererMapLowercase = Object.keys(rendererMap).reduce((acc, key) => {
+  acc[key.toLowerCase()] = rendererMap[key];
+  return acc;
+}, {});
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+    ...rendererMapLowercase,
+  };
 }
