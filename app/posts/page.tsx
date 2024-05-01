4139b41c64baa742dc2d34e09d42457625626bf6
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-01 09:38:08 -0400

    chore: metadata, cleanup, move post to new path

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 import { NextPageWithLayout } from "lib/types";
 import { getAllPostsMap } from "lib/api";
 import { ArticleCategoryCard } from "components/ArticleCategoryCard";
-import { Meta } from "components/meta";
+import type { Metadata } from 'next'
 import { CardGrid } from "components/CardGrid";
 import MoreStories from "components/more-stories";
 
@@ -13,25 +13,31 @@ export type PostsIndexPageProps = NextPageWithLayout & {
   allPosts: Record<string, any>;
 };
 
-export const ARTICLE_INDEX_CARDS = [
-  {
-    title: "⚛️ Full-Stack React",
-    description:
-      "I write about React, Next.js and other full-stack Jamstack topics at cutintothejamstack.com.",
-    ctaText: "cutintothejamstack.com",
-    ctaHref: "https://cutintothejamstack.com",
-  },
-  {
-    title: "🛣 Tech Strategy",
-    description: "I've written about various tech topics on the Echobind Blog.",
-    ctaText: "Echobind Blog",
-    ctaHref: "https://echobind.com/team/mike-cavaliere",
-  },
-  {
-    title: "👔 Freelance Programming",
-    description: "Older articles on freelancing as a programmer are below. ",
-  },
-];
+export const metadata: Metadata = {
+  title: "Mike Cavaliere's articles on JavaScript, React, technical strategy, and brain hacking.",
+  description: "Over the years I've written about software, tech careers, and personal development.",
+}
+
+
+// export const ARTICLE_INDEX_CARDS = [
+//   {
+//     title: "⚛️ Full-Stack React",
+//     description:
+//       "I write about React, Next.js and other full-stack Jamstack topics at cutintothejamstack.com.",
+//     ctaText: "cutintothejamstack.com",
+//     ctaHref: "https://cutintothejamstack.com",
+//   },
+//   {
+//     title: "🛣 Tech Strategy",
+//     description: "I've written about various tech topics on the Echobind Blog.",
+//     ctaText: "Echobind Blog",
+//     ctaHref: "https://echobind.com/team/mike-cavaliere",
+//   },
+//   {
+//     title: "👔 Freelance Programming",
+//     description: "Older articles on freelancing as a programmer are below. ",
+//   },
+// ];
 
 export default async function PostsIndexPage() {
   const allPostsMap = await getAllPostsMap();
@@ -39,19 +45,13 @@ export default async function PostsIndexPage() {
 
   return (
     <>
-      {/* <Meta
-        titlePrefix="Mike Cavaliere's articles on JavaScript, React, technical strategy, and brain hacking."
-        titleSuffix=""
-        titleSeparator=""
-        description="Over the years I've written about software, tech careers, and brain improvement."
-      /> */}
       <Box mb={10} textAlign="center">
         <Heading1 mb={3}>My Articles</Heading1>
         <Heading2 mb={10}>
           Over the years I've written about software, tech careers, and brain improvement.
         </Heading2>
 
-        <CardGrid>
+        {/* <CardGrid>
           {ARTICLE_INDEX_CARDS.map(({ title, description, ctaHref, ctaText }) => (
             <ArticleCategoryCard
               key={title}
@@ -61,7 +61,7 @@ export default async function PostsIndexPage() {
               ctaHref={ctaHref}
             />
           ))}
-        </CardGrid>
+        </CardGrid> */}
       </Box>
       <MoreStories posts={allPosts} />
     </>
