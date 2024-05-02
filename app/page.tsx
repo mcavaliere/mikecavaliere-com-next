224ee0a754225b626e818f97808777d7aef069bd
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-02 13:41:59 -0400

    feat: post metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
-import Head from "next/head";
 import { HomeHero } from "components/HomeHero";
 import { HomeSectionsGrid } from "components/HomeSectionsGrid";
-import { Meta } from "components/meta";
-import { NextPageWithLayout } from "lib/types";
-import { getAllPostsMap } from "../lib/api";
-import { CMS_NAME } from "../lib/constants";
+import type { Metadata } from "next";
 
-export type IndexPageProps = NextPageWithLayout & {};
+export const metadata: Metadata = {
+  title: "Mike Cavaliere: software engineer, conference speaker, author.",
+  description: "Over the years I've written about software, tech careers, and brain improvement.",
+};
 
-export default function IndexPage({}: IndexPageProps) {
+export default function IndexPage() {
   return (
     <>
-      {/* <Meta
-        titlePrefix="Mike Cavaliere: software engineer, conference speaker, author."
-        titleSuffix=""
-        titleSeparator=""
-        description="Over the years I've written about software, tech careers, and brain improvement."
-      /> */}
       <HomeHero />
 
       <HomeSectionsGrid />
--- app/posts/(post)/[slug]/page.tsx
+++ app/posts/(post)/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata, ResolvingMetadata } from 'next'
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getAllPostsMap, getPostContent, getPostFrontMatter } from "@/lib/api";
 import { rendererMapLowercase } from "@/lib/postRenderers";
@@ -7,6 +8,30 @@ export async function generateStaticParams() {
   const map = await getAllPostsMap();
   const params = Object.values(map)
   return params;
+}
+
+type Props = {
+  params: { slug: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+
+  // fetch data
+  const frontmatter = await getPostFrontMatter(slug)
+
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.excerpt,
+    openGraph: frontmatter.featuredImage?.src ? {
+      images: [frontmatter.featuredImage?.src],
+    } : undefined,
+  }
 }
 
 export default async function ArticlePage({ params: { slug } }) {
