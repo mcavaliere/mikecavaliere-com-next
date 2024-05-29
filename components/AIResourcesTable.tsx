55fe39ae6c2f54d4a982f1d8f8e3940730e212f7
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-29 14:20:52 -0400

    feat: base content

--- components/AIResourcesTable.tsx
+++ components/AIResourcesTable.tsx
@@ -1,17 +1,49 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from "@chakra-ui/react";
+import { Link } from "@/components/Link";
 
-import tools from "../data/ai-resources.json";
+import resources from "../data/ai-resources.json";
 
 export function AIResourcesTable() {
-  return <h1>AI RESOURCES TABLE</h1>;
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Description</Th>
+            <Th>Tags</Th>
+            <Th>Links</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {resources.map((r, i) => {
+            const href = r.githubUrl ? r.githubUrl : r.websiteUrl;
+            return (
+              <Tr key={i}>
+                <Td>
+                  <Link href={href} target="_blank" textDecoration="underline">
+                    {r.name}
+                  </Link>
+                </Td>
+                <Td>{r.description}</Td>
+                <Td>{r.tags.join(", ")}</Td>
+                <Td>
+                  {r.websiteUrl && (
+                    <Link href={r.websiteUrl} target="_blank" textDecoration="underline">
+                      Website
+                    </Link>
+                  )}
+                  {r.githubUrl && (
+                    <Link href={r.githubUrl} target="_blank" textDecoration="underline">
+                      GitHub
+                    </Link>
+                  )}
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
