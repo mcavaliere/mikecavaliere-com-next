9cc6980817cfea62d1d77e4f454ffadf1cea626b
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-29 14:11:18 -0400

    chore: stub page and mdx component

---
+++ components/AIResourcesTable.tsx
@@ -0,0 +1,17 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+
+import tools from "../data/ai-resources.json";
+
+export function AIResourcesTable() {
+  return <h1>AI RESOURCES TABLE</h1>;
+}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,11 @@
 import type { MDXComponents } from "mdx/types";
 import { rendererMapLowercase } from "./lib/postRenderers";
-
-
+import { AIResourcesTable } from "./components/AIResourcesTable";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     ...rendererMapLowercase,
+    AIResourcesTable,
   };
 }
