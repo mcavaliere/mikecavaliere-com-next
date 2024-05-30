cb4cf758d4f0792738dfc6afa00d005388a38587
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-30 14:31:09 -0400

    feat: about page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,64 +1,50 @@
-import {
-  Box,
-  Center,
-  Image as ChakraImage,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import { Box, Center, SimpleGrid } from "@chakra-ui/react";
 import { Heading1, P } from "components/Headings";
 import { ContactLinks } from "components/ContactLinks";
 import { ParagraphLink } from "components/Link";
+import Image from "next/image";
+import photo from "/public/images/mike-cavaliere-in-office.jpg";
 
 export default function AboutPage() {
   return (
     <>
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
-        <ChakraImage
-          src="/images/mike-cavaliere-in-office.jpg"
-          borderRadius={10}
-          overflow="hidden"
-          width="100%"
-          boxShadow="2xl"
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Image
+          alt="Mike Cavaliere in his office, smiling at the camera."
+          src={photo}
+          className="rounded-lg overflow-hidden shadow-2xl"
         />
 
         <Box paddingLeft={10}>
-          <Heading1
-            mb={1}
-            pos="relative"
-            _before={{ content: '"👋🏼"', position: "absolute", left: "-40px" }}
-          >
+          <Heading1 className="relative before:content-['👋🏼'] before:absolute before:left-[-40px] mb-3">
             Hi, I&#39;m Mike.
           </Heading1>
-          <P mb={5}>
-            I started playing with computers when I was a kid, and never
-            stopped. Since then I&#39;ve written thousands of lines of code, built
-            websites and applications galore, and worked with dozens upon dozens
-            of bright people in the tech world.
-          </P>
-          <P mb={5}>
-            I&#39;ve been a full-stack software engineer for many years. I&#39;ve also
-            managed and mentored others, and continue to do so.
-          </P>
-          <P mb={5}>
-            Somewhere along the way, I discovered that I love to write and
-            present. Since then I&#39;ve written dozens of{" "}
-            <ParagraphLink href="/posts">articles</ParagraphLink>, been on a
-            number of{" "}
-            <ParagraphLink href="/talks#interviews">podcasts</ParagraphLink>,
-            given a number of{" "}
-            <ParagraphLink href="/talks#interviews">talks</ParagraphLink> at
-            conferences and meetups, and recently wrote{" "}
+          <p className="mb-3 text-lg">
+            I started playing with computers when I was a kid, and never stopped. Since then
+            I&#39;ve written thousands of lines of code, built websites and applications galore, and
+            worked with dozens upon dozens of bright people in the tech world.
+          </p>
+          <p className="mb-3 text-lg">
+            I&#39;ve been a full-stack software engineer for many years. I&#39;ve also managed and
+            mentored others, and continue to do so.
+          </p>
+          <p className="mb-3 text-lg">
+            Somewhere along the way, I discovered that I love to write and present. Since then
+            I&#39;ve written dozens of <ParagraphLink href="/posts">articles</ParagraphLink>, been
+            on a number of <ParagraphLink href="/talks#interviews">podcasts</ParagraphLink>, given a
+            number of <ParagraphLink href="/talks#interviews">talks</ParagraphLink> at conferences
+            and meetups, and recently wrote{" "}
             <ParagraphLink href="https://cutintothejamstack.com">
               my first software book
             </ParagraphLink>
             .
-          </P>
+          </p>
 
-          <P mb={5}>
-            I plan to continue sharing my knowledge, and hope you find it
-            useful.
-          </P>
+          <p className="mb-3 text-lg">
+            I plan to continue sharing my knowledge, and hope you find it useful.
+          </p>
         </Box>
-      </SimpleGrid>
+      </div>
       <Center>
         <ContactLinks />
       </Center>
