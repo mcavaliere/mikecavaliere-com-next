5c9ef8a4eabe40123b9ce307c67e3d1786d43c7f
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-30 16:16:27 -0400

    fix: gap

--- components/CardGrid.tsx
+++ components/CardGrid.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
 export function CardGrid({ children, className = "", ...rest }) {
-  const classNames = cn("grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3", className);
+  const classNames = cn("grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3", className);
   return <div className={classNames}>{children}</div>;
 }
