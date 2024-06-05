1597b349e7ce1cd35e2aa8c56e2a80068f271b71
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-06-05 10:58:39 -0400

    fix: default style for links

--- components/Link.tsx
+++ components/Link.tsx
@@ -4,7 +4,7 @@ import { default as NextLink } from "next/link";
 import { cn } from "@/lib/utils";
 
 export const Link = ({ children, className = "", href, ...props }) => {
-  const classNames = cn("underline hover:text-primary", className);
+  const classNames = cn("underline text-foreground hover:text-primary", className);
 
   return (
     <NextLink href={href} className={classNames} {...props}>
