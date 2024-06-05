8870f1f9f807fdb8ff773f1ffc6f8d15fe6440db
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-06-05 10:58:30 -0400

    feat: update back link

--- components/BackLink.tsx
+++ components/BackLink.tsx
@@ -1,21 +1,15 @@
 import { Link } from "@/components/Link";
+import { cn } from "@/lib/utils";
 import { FaArrowLeft } from "react-icons/fa";
-export function BackLink({ href, ...props }: { href: string; [key: string]: any }) {
+export function BackLink({ href, className = "", ...props }: { href: string; [key: string]: any }) {
+  const classNames = cn(
+    "flex flex-row items-center font-semibold no-underline hover:text-foreground hover:opacity-60 mb-8",
+    className
+  );
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        marginBottom: "20px",
-        fontWeight: "semibold",
-      }}
-    >
-      <Link href={href} display="flex" flexDirection="row" alignItems="center" fontWeight="semibold" {...props}>
-        <FaArrowLeft style={{ marginRight: "5px" }} />
-        Back
-      </Link>
-    </div>
+    <Link href={href} className={classNames} {...props}>
+      <FaArrowLeft className="mr-3" />
+      <span>Back</span>
+    </Link>
   );
 }
