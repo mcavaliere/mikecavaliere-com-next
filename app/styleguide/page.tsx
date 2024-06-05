b626004709b6b258b42aa2676ccc60c29d58371d
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-06-05 11:15:41 -0400

    feat: styleguide page w/colors

---
+++ app/styleguide/page.tsx
@@ -0,0 +1,23 @@
+import { Heading1, Heading2, P } from "@/components/Headings";
+
+export default function Styleguide() {
+  return (
+    <div>
+      <Heading1 className="mb-5">Styleguide</Heading1>
+
+      <P>I&#39;ll centralize styles and UI elements here.</P>
+
+      <Heading2 className="mb-3">Colors</Heading2>
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="bg-primary p-4 text-primary-forground">Primary</div>
+        <div className="bg-secondary p-4 text-secondary-foreground">Secondary</div>
+        <div className="bg-muted p-4 text-muted-foreground">Muted</div>
+        <div className="bg-destructive p-4 text-destructive-foreground">Destructive</div>
+        <div className="bg-accent p-4 text-accent-foreground">Accent</div>
+        <div className="bg-popover p-4 text-popover-foreground">Popover</div>
+        <div className="bg-card p-4 text-card-foreground">Card</div>
+      </div>
+    </div>
+  );
+}
