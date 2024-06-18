67bb79a3ce407f5ab6c42c0d5c9e15f4b8ed2e78
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-06-18 15:51:22 -0400

    feat: opaque navbar on scroll

--- layouts/SiteOuterLayout.tsx
+++ layouts/SiteOuterLayout.tsx
@@ -1,14 +1,34 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, SyntheticEvent, useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 
 export type SiteOuterLayoutProps = {
   children: ReactNode;
 };
 
 export const SiteOuterLayout = ({ children, ...props }: SiteOuterLayoutProps) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const scrolled = window.scrollY > 0;
+    setScrolled(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const classNames = cn("bg-transparent transition-all", scrolled ? "bg-background shadow-md" : "");
+
   return (
     <div {...props}>
-      <Navbar className="bg-transparent" />
+      <Navbar className={classNames} />
       {children}
     </div>
   );
