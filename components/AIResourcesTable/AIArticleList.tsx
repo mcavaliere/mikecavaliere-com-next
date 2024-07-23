962dbc7986a96fb32e9d14ca9ec70e028c2005d7
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-07-23 14:00:14 -0400

    feat: styling for articles

--- components/AIResourcesTable/AIArticleList.tsx
+++ components/AIResourcesTable/AIArticleList.tsx
@@ -6,7 +6,9 @@ import { AIResource } from "./types";
  * Container component for AI for JS Devs article list.
  */
 export function AIArticleList() {
-  const articles = AIResources.filter((r) => ["articles", "learning"].includes(r.category));
+  const articles = AIResources.filter(
+    (r) => ["articles", "learning"].includes(r.category) && !!r.websiteUrl
+  ).sort((a, b) => (a.name > b.name ? 1 : -1));
 
   return <ArticleList articles={articles as AIResource[]} />;
 }
--- components/AIResourcesTable/ArticleList.tsx
+++ components/AIResourcesTable/ArticleList.tsx
@@ -1,13 +1,24 @@
 import { AIResource } from "./types";
+import Link from "next/link";
+import { Heading2 } from "../Headings";
+import { SquareArrowUpRight } from "lucide-react";
 
 export function ArticleList({ articles }: { articles: AIResource[] }) {
   return (
-    <ul>
+    <ul className="flex flex-col gap-2">
       {articles.map((article) => (
         <li key={article.name}>
-          <h2>{article.name}</h2>
-          <p>{article.description}</p>
-          <p>{article.addedOn}</p>
+          <Link
+            href={article.websiteUrl as string}
+            target="_blank"
+            className="flex flex-row items-center gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent bg-muted"
+          >
+            <div className="flex-1">
+              <Heading2 className="text-lg">{article.name}</Heading2>
+              <p>{article.description}</p>
+            </div>
+            <SquareArrowUpRight className="w-4 h-4" />
+          </Link>
         </li>
       ))}
     </ul>
