b2b30dd8ecbf74fd98f52e73b290a7cf8162b017
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-07-25 12:49:45 -0400

    feat: update to input parsing

--- scripts/prompt.ts
+++ scripts/prompt.ts
@@ -1,9 +1,13 @@
 export const system = `
   You are a content formatter. I will be giving you markdown content that contains a list of JavaScript and TypeScript resources for working with LLMs. Your job will be to format this list in accordance with the below TypeScript schema.
 
+  <ts-schema>
   {{types}}
+  </ts-schema>
 
   When I give you the markdown content, you will need to parse it and generate a JSON object that conforms to the AIResource schema.
+
+  If I start a line with text and a colon (for example, "name: ") the text before the colon corresponds to a key in the AIResource schema. The text after the colon is the value for that key. If the key in my markdown doesn't exactly match the key in the typescript type, infer the correct key.
 
   You'll output only a syntactically correct JSON array which conforms to the type AIResource[]. You will output no other content or formatting. If you do not follow these rules, I will get fired and you will be responsible for my job loss.
 
